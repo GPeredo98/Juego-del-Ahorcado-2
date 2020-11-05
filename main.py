dc9590d175db9e9eb1391d47c1b955d5f91e70fe
javiervpr <javiervacapereiraroca@gmail.com>
2020-11-04 21:23:49 -0400

    Estructura proyecto

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
-# Inicio del juego
+versions = []
+states = {}
+"""
+states {
+    "v1": {
+        "palabras_disponibles": ["fdask", "fdsfdas"],
+        "palabra_secreta": None,
+        "palabra_mostrada": None,
+        "letras_encontradas": ['a','b'],
+        "vidas": 5
+    }
+}
+"""
+
+
+def get_action(function, args):
+    """ Funcion contenedora
+    """
+    return function(args)
+
+
+def get_last_version(args):
+    """ Obtiene la ultima version de los estados
+    """
+    if args['versions'].__len__() == 0:
+        return args['versions']
+    return args['versions'][-1]
+
+
