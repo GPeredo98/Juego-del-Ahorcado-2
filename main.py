0bfb04b5c739d25d563a00793c1dc7e9ee359ed3
Gabriel Peredo <gabriel.peredo777@gmail.com>
2020-11-04 21:23:02 -0400

    funciones

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
+import itertools
 # Inicio del juego
 
 # verificarEntrada
 # verificarLetraRepetida
-# quitarVidas
 # cargarPalabrasSecretas
 # seleccionarPalabra
 # verificarSiEsNumero
 # destaparPalabra
 # verificarLetrasOcultas
+
+
+
+def quitar_vidas(letra, palabra_secreta, vidas):
+    pass
+    #coincide = False
+    #letras_de_palabra = palabra_secreta.toCharArray()
+
