2d57998ade7a32df359accc015527d3dc22f6165
Gabriel Peredo <gabriel.peredo777@gmail.com>
2020-11-04 21:27:06 -0400

    archivo aparte

---
+++ functions.py
@@ -0,0 +1,16 @@
+import itertools
+# Inicio del juego
+
+# verificarEntrada
+# verificarLetraRepetida
+# cargarPalabrasSecretas
+# seleccionarPalabra
+# verificarSiEsNumero
+# destaparPalabra
+# verificarLetrasOcultas
+
+
+def quitar_vidas(letra, palabra_secreta, vidas):
+    pass
+    #coincide = False
+    #letras_de_palabra = palabra_secreta.toCharArray()
