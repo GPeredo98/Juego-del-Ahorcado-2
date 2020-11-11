20e19aa2fb6e56a29006c47cc3e3022a59bcb287
Gabriel Peredo <gabriel.peredo777@gmail.com>
2020-11-11 15:32:56 -0400

    feat: funciones

--- functions.py
+++ functions.py
@@ -10,7 +10,24 @@ import itertools
 # verificarLetrasOcultas
 
 
-def quitar_vidas(letra, palabra_secreta, vidas):
+# letra, palabra_secreta, vidas
+def quitar_vidas(args):
+    obj = args['states'][args['last_version']]
+    letra = args['letra']
+    coincide = False
+    letras_de_palabra = list(itertools.chain.from_iterable(obj['palabra_secreta']))
+
+    # Buscamos si la letra coincide con alguna de la palabra oculta
+    aux = [x for x in letras_de_palabra if x == letra]
+    if len(aux) == 0:
+        obj['vidas'] = obj['vidas'] - 1
+
+    return obj
+
+
+def verificar_si_es_numero(args):
     pass
-    #coincide = False
-    #letras_de_palabra = palabra_secreta.toCharArray()
+
+
+def destapar_palabra(args):
+    pass
